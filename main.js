977c87e7df9b3efe6dee5b9a74a6b622292db825
Leif <leifj66@gmail.com>
2024-03-19 08:28:23 +0100

    Nearlu finished pet card feature

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ async function petsArea() {
     const clone = template.content.cloneNode(true)
 
     clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
 
     wrapper.appendChild(clone)
   })
@@ -24,3 +26,13 @@ async function petsArea() {
 }
 
 petsArea()
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear()
+  const age = currentYear - birthYear
+
+  if (age == 1) return "1 year old"
+  if (age == 0) return "Less than a year old"
+
+  return `${age} years old`
+}
