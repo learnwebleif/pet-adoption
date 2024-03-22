c4fcfa6f474c5411e2b495fa81a0ee45c2a1d8ab
Leif <leifj66@gmail.com>
2024-03-22 14:53:27 +0100

    Finished pet filtering

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ const wrapper = document.createDocumentFragment()
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
   const weatherData = await weatherPromise.json()
+
   const ourTemperature = weatherData.properties.periods[0].temperature
   document.querySelector("#temperature-output").textContent = ourTemperature
+
 }
 
 start()
@@ -16,12 +18,16 @@ async function petsArea() {
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
 
+    clone.querySelector(".pet-card").dataset.species = pet.species
+
     clone.querySelector("h3").textContent = pet.name
     clone.querySelector(".pet-description").textContent = pet.description
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+
     if (!pet.photo) pet.photo = "images/fallback.jpg"
+
     clone.querySelector(".pet-card-photo img").src = pet.photo
-    clone.querySelector(".pet-card-photo img").alt = `a ${pet.species} named ${pet.name}`
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
 
     wrapper.appendChild(clone)
   })
@@ -51,8 +57,16 @@ function handleButtonClick(e) {
   // remove active class from any and all buttons
   allButtons.forEach(el => el.classList.remove("active"))
 
-  // add active class to button just got clicked
+  // add active class to the specific button that just got clicked
   e.target.classList.add("active")
 
   // actually filter the pets down below
+  const currentFilter = e.target.dataset.filter
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid"
+    } else {
+      el.style.display = "none"
+    }
+  })
 }
